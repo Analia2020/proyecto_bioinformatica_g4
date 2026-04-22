a3af83e7ec8bd6667d26d0f63bb369de2f26bf38
Caren Moreno <carenmoreno28@gmail.com>
2026-04-22 00:39:01 -0300

    Descripción de los datos descargados

--- src/src/01_descarga_datos.R
+++ src/src/01_descarga_datos.R
@@ -3,3 +3,9 @@ r# =============================================================================
 # Proyecto: Análisis de Expresión Génica Diferencial en Cáncer de Mama
 # Autores: Grupo 4 - UNIR
 # Fecha: 22/04/2025
+# =============================================================================
+# Descripción:
+# Este script descarga los datos de expresión génica del repositorio público
+# GEO (Gene Expression Omnibus) usando el acceso GSE45827.
+# Los datos corresponden a muestras de tejido tumoral y tejido sano de mama.
+# =============================================================================
