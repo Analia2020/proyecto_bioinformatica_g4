3eba4f3d7b1679a3db458a61c01894c2b268d117
Caren Moreno <carenmoreno28@gmail.com>
2026-04-22 00:35:02 -0300

    Creación 01_descarga_datos.R
    Script para descargar los datos de GEO.

---
+++ src/src/01_descarga_datos.R
@@ -0,0 +1,5 @@
+r# =============================================================================
+# SCRIPT 01: Descarga de datos desde GEO (NCBI)
+# Proyecto: Análisis de Expresión Génica Diferencial en Cáncer de Mama
+# Autores: Grupo 4 - UNIR
+# Fecha: 22/04/2025
